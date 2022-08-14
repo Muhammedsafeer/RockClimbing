0e900e5778b5a22bd6f23f5eecb623d6501569f4
Muhammedsafeer <muhammedsafeer.shefin.swafa@gmail.com>
2022-08-14 15:25:18 +0530

    Added Music 1
    Added Clicking sound to the game.

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ function Name_correct() {
             document.getElementById("play_buttonX").id="play_button";
             document.getElementById("SettingsX").id="Settings";
             document.getElementById("HelpX").id="Help";
+            let click = document.getElementById("click");
+            click.play()
         }
     }
     if (length > 18) {
@@ -36,6 +38,8 @@ document.addEventListener("keypress", function(event) {
                 document.getElementById("play_buttonX").id="play_button";
                 document.getElementById("SettingsX").id="Settings";
                 document.getElementById("HelpX").id="Help";
+                let click = document.getElementById("click");
+                click.play()
             }
         }
         if (length > 18) {
@@ -45,4 +49,25 @@ document.addEventListener("keypress", function(event) {
             document.getElementById("correct_or_not").innerHTML = "Your Name Has to be Above 3 Characters"
         }
     }
-  });
+  }
+);
+
+
+function play() {
+
+}
+
+function settings() {
+
+}
+
+function help() {
+    document.getElementById("play_button").id="play_buttonX";
+    document.getElementById("Settings").id="SettingsX";
+    document.getElementById("Help").id="HelpX"
+    document.getElementById("InHelpX").id="InHelp";
+
+
+    let click = document.getElementById("click");
+    click.play()
+}
