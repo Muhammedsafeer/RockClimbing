c4759b5f9ec194742c058710daed6c841bea1ab8
Muhammedsafeer <muhammedsafeer.shefin.swafa@gmail.com>
2022-08-14 13:59:57 +0530

    Update
    Made background for game and keybourd shotcuts

--- ReadME.md
+++ ReadME.md
@@ -6,6 +6,7 @@ This is a Game called RockClimbing
 ## Credit :-
 Game Name from :- Faheem <br />
 Game Creater :- Muhammed Safeer
+Language :- En
 
 
 
--- script.js
+++ script.js
@@ -8,6 +8,10 @@ function Name_correct() {
             document.getElementById("name_enter").id="name_enterX";
             document.getElementById("NameIN").innerHTML = "";
             document.getElementById("correct_or_not").innerHTML = "";
+            document.getElementById("BackgroundX").id="Background";
+            document.getElementById("play_buttonX").id="play_button";
+            document.getElementById("SettingsX").id="Settings";
+            document.getElementById("HelpX").id="Help";
         }
     }
     if (length > 18) {
@@ -17,3 +21,28 @@ function Name_correct() {
         document.getElementById("correct_or_not").innerHTML = "Your Name Has to be Above 3 Characters"
     }
 }
+document.addEventListener("keypress", function(event) {
+    if (event.keyCode == 13) {
+        const Name = document.getElementById("name_input").value;
+        const length = Name.length;
+        
+        if (length <18) {
+            if (length >3){
+                document.getElementById("name_input").id="name_inputX";
+                document.getElementById("name_enter").id="name_enterX";
+                document.getElementById("NameIN").innerHTML = "";
+                document.getElementById("correct_or_not").innerHTML = "";
+                document.getElementById("BackgroundX").id="Background";
+                document.getElementById("play_buttonX").id="play_button";
+                document.getElementById("SettingsX").id="Settings";
+                document.getElementById("HelpX").id="Help";
+            }
+        }
+        if (length > 18) {
+            document.getElementById("correct_or_not").innerHTML =  "Your Name Has to be Under 18 Characters";
+        }
+        if (length < 3) {
+            document.getElementById("correct_or_not").innerHTML = "Your Name Has to be Above 3 Characters"
+        }
+    }
+  });
