e5ee3ec71f5eba45a4abe64d6b9d98aa7d56c3f8
Muhammedsafeer <muhammedsafeer.shefin.swafa@gmail.com>
2022-08-14 11:13:43 +0530

    Update
    Updated - app.html, ReadME.md, script.js, style.css

--- ReadME.md
+++ ReadME.md
@@ -6,3 +6,12 @@ This is a Game called RockClimbing
 ## Credit :-
 Game Name from :- Faheem <br />
 Game Creater :- Muhammed Safeer
+
+
+
+
+
+
+# Time
+Game Making Started on                      8/13/2022 <br />
+Game Name Changing Finished on              8/14/2022 <br />
--- script.js
+++ script.js
@@ -6,9 +6,14 @@ function Name_correct() {
         if (length >3){
             document.getElementById("name_input").id="name_inputX";
             document.getElementById("name_enter").id="name_enterX";
+            document.getElementById("NameIN").innerHTML = "";
+            document.getElementById("correct_or_not").innerHTML = "";
         }
     }
     if (length > 18) {
-        console.log("No")
+        document.getElementById("correct_or_not").innerHTML =  "Your Name Has to be Under 18 Characters";
+    }
+    if (length < 3) {
+        document.getElementById("correct_or_not").innerHTML = "Your Name Has to be Above 3 Characters"
     }
 }
